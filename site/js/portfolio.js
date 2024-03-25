73f4680dea42b60dab35b3d0dbe3e3e4de78affc
iSayZ <estrine.alexis@gmail.com>
2024-03-25 11:52:35 +0100

    finish JS step 5 in part 2

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -3,6 +3,7 @@ let descriptionName = document.querySelector("#firstname")
 let descriptionBg = document.querySelector(".description")
 let footerBg = document.querySelector("footer.pink-bg")
 let pinkText = document.querySelectorAll(".pink-text")
+let navLink = document.querySelectorAll("header nav a")
 let btnModifyTextColor = document.querySelector(".modifyTextColor")
 
 
@@ -11,11 +12,14 @@ avatarBanner.addEventListener("click", () => {
 })
 
 btnModifyTextColor.addEventListener("click", () => {
-    let newColor = prompt("What color do you want for the background ?")
+    let newColor = prompt("What color do you want ?")
     descriptionBg.style.backgroundColor = newColor
     footerBg.style.backgroundColor = newColor
     for (let text of pinkText) {
         text.style.color = newColor
+    }
+    for (let link of navLink) {
+        link.style.color = newColor
     }
     descriptionName.innerHTML = prompt("What's your name ?")
     descriptionName.style.color = "white"
