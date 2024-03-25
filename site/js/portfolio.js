7f88399b71be9f7dc4812d357ed48731ffc32fe5
PMorganGit <morganpouilly@gmail.com>
2024-03-25 13:51:31 +0100

    description color/name

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -3,7 +3,22 @@ const avatarlogo = document.querySelector(".logohead");
 avatarlogo.addEventListener("click", () => {
   const img = document.querySelector(".logohead");
 
-  img.src = "./image/avatar";
+  img.src = "./image/avatar.svg";
 });
 
+const changeName = document.querySelector(".buttonModifer");
+changeName.addEventListener("click", () => {
 
+
+const changeColor = prompt("Change color.");
+const ancienneColor = document.querySelector(".description.pink-bg");
+
+ancienneColor.style.backgroundColor = changeColor;
+
+const changeNom = prompt("Put your name.");
+const ancienNom = document.getElementById("firstname");
+const nouveauNom = changeNom;
+
+ancienNom.replaceWith(nouveauNom);
+
+});
