1c417ece21b2f8c3dcee6a36f6a4c6e01be80002
iSayZ <estrine.alexis@gmail.com>
2024-03-25 12:10:57 +0100

    finish JS step 7 in part 2

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -5,10 +5,14 @@ let descriptionBg = document.querySelector(".description")
 let footerBg = document.querySelector("footer.pink-bg")
 let pinkText = document.querySelectorAll(".pink-text")
 let navLink = document.querySelectorAll("header nav a")
-let btnModifyTextColor = document.querySelector(".modifyTextColor")
+let btnModifyTextColor = document.querySelector("#modifyTextColor")
 
 let frontEndDevToolsList = document.querySelector("#front-dev-tools")
 let btnModifyFrontDevTools = document.querySelector("#modifyFrontDevTools")
+
+let backEndDevToolsList = document.querySelector("#back-dev-tools")
+let inputDevToolsBack = document.querySelector("#inputDevToolsBack")
+let btnAddDevToolsBack = document.querySelector("#addDevToolsBack")
 
 avatarBanner.addEventListener("click", () => {
     avatarBanner.src = "image/avatar.svg"
@@ -30,4 +34,11 @@ btnModifyTextColor.addEventListener("click", () => {
 
 btnModifyFrontDevTools.addEventListener("click", () => {
     frontEndDevToolsList.innerHTML = `<li>VsCode</li><li>Github</li><li>Terminal</li>`
+})
+
+let newDevToolsBack = `<li>VsCode</li>`
+
+btnAddDevToolsBack.addEventListener("click", () => {
+    newDevToolsBack = newDevToolsBack + `<li>${inputDevToolsBack.value}</li>`
+    backEndDevToolsList.innerHTML = newDevToolsBack
 })
