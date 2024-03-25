dfef8d09482b0e228c94b5e349ae717554858fab
AUDREY BAUDRY <baudry.audrey@outlook.fr>
2024-03-25 11:33:31 +0100

    step 7 et 8 css + une partie de la step 4 de js

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -16,11 +16,14 @@ btn.innerHTML = "Modifier le nom et la couleur";
 let zone = document.querySelector(".description");
 zone.appendChild(btn);
 let nameToBeReplaced = document.getElementById("firstname");
+let footer = document.querySelector("footer");
+let text = document.querySelectorAll("pink-text");
 
 btn.addEventListener("click", function () {
   let colorUser = prompt(`Enter a color`);
   let nameUser = prompt(`What's your name ?`);
   zone.style.backgroundColor = colorUser;
+  footer.style.backgroundColor = colorUser;
   nameToBeReplaced.innerHTML = nameUser;
   document.getElementById("firstname").style.color = "white";
 });
