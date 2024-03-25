0076c54e073fe11b2c4b5a1c3617c1419295adb6
PMorganGit <morganpouilly@gmail.com>
2024-03-25 12:35:49 +0100

    algo 1 2 3 et css fait

---
+++ site/js/portfolio.js
@@ -0,0 +1,9 @@
+const avatarlogo = document.querySelector(".logohead");
+
+avatarlogo.addEventListener("click", () => {
+  const img = document.querySelector(".logohead");
+
+  img.src = "./image/avatar";
+});
+
+
