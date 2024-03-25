e56a550ebb4f00be31d78c86f2e94231da906632
iSayZ <estrine.alexis@gmail.com>
2024-03-25 11:30:41 +0100

    finish JS step 2 in part 2

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,5 +1,12 @@
 let avatarBanner = document.querySelector(".banner-avatar")
+let descriptionName = document.querySelector("#firstname")
+let btnModifyTextColor = document.querySelector(".modifyTextColor")
 
 avatarBanner.addEventListener("click", () => {
     avatarBanner.src = "image/avatar.svg"
+})
+
+btnModifyTextColor.addEventListener("click", () => {
+    descriptionName.innerHTML = prompt("What's your name ?")
+    descriptionName.style.color = "white"
 })
