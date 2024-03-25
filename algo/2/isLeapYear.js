a01671f7ead9c37d4f67591f14febfdddde010c4
alexandre michiels <alexoualexandre1@gmail.com>
2024-03-25 09:32:51 +0100

    finish algo 2 isLeapYear.js

--- algo/2/isLeapYear.js
+++ algo/2/isLeapYear.js
@@ -8,7 +8,9 @@ Rappel : Pour vérifier si un nombre est divisible par un autre, tu peux utilise
 */
 
 function isLeapYear(year) {
-  // Your code here !
+  let result;
+  year % 4 == 0 && year % 400 == 0 && !year % 100 == 0 ?  result = true : result = false;
+  return result 
 }
 
 module.exports = isLeapYear;
